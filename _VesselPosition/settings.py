839d8e6e064e962dc470f9ef4bc8a0675c993fe8
Alakar-ISS <alakar_harijan@outlook.com>
2025-07-01 09:23:32 +0530

    changes

--- _VesselPosition/settings.py
+++ _VesselPosition/settings.py
@@ -137,16 +137,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'signin'
 
-EMAIL_HOST="smtp.gmail.com"
-EMAIL_PORT=465
-EMAIL_USE_SSL=True
-EMAIL_HOST_USER="alakar.harijan2000@gmail.com"
-EMAIL_HOST_PASSWORD="vmzv ndwr goky laor"
-
 # EMAIL_HOST="smtp.gmail.com"
 # EMAIL_PORT=465
 # EMAIL_USE_SSL=True
-# EMAIL_HOST_USER="donot.reply.automail1234@gmail.com"
-# EMAIL_HOST_PASSWORD="tdgm quqd stti zwqx"
+# EMAIL_HOST_USER="alakar.harijan2000@gmail.com"
+# EMAIL_HOST_PASSWORD="vmzv ndwr goky laor"
+
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=465
+EMAIL_USE_SSL=True
+EMAIL_HOST_USER="donot.reply.automail1234@gmail.com"
+EMAIL_HOST_PASSWORD="zwka yvht ridi avjr"
 
 AUTH_USER_MODEL = 'App.CustomUser'
